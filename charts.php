12c2d396a86484d8f3a27fb93697078edd37f841
novomir <34912258+novomirr@users.noreply.github.com>
2025-07-01 16:33:22 +0500

    Add files via upload

---
+++ charts.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Графики</title>
+	<link href="web/css/bootstrap.css" rel="stylesheet">
+    <link href="web/css/style.css" rel="stylesheet">
+	<link href="web/css/charts.css" rel="stylesheet">
+	<script src="web/js/jquery-3.6.0.min.js"></script>
+	<script src="web/js/d3.min.js"></script>
+</head>
+<body>
+	<div class="container-fluid m-0 p-0">
+		<div class="row no-gutters my-5">
+			<svg width="300" height="300" id="chart"></svg>
+		</div>
+
+        <div class="row no-gutters my-5" id="pie">
+        </div>
+	</div>
+<script src="web/js/charts.js"></script>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,404 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Bild Site Sells</title>
+	<link href="web/css/bootstrap.css" rel="stylesheet">
+	<link href="web/css/style.css" rel="stylesheet">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+	<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+	<script src="web/js/jquery.scrollify.js"></script>
+	<script src="web/js/jquery-migrate-1.2.1.min.js"></script>
+	<script src="web/js/jquery.easing.1.3.js"></script>
+	<script src="web/js/jquery.maskedinput.js"></script>
+	<script src="web/js/jquery.mousewheel.min.js"></script>
+</head>
+<body>
+	<div class="section" id="1">
+
+		<div class="lines" id="l_1"></div>
+		<div class="lines" id="l_2"></div>
+
+		<div class="container-fluid p-0 h-100">
+			<div class="row no-gutters h-100">
+				<div class="col-11 m-auto p-0 h-100">
+					<div class="row no-gutters h-100">
+						<div class="col-11 m-auto p-0 h-100">
+
+							<!-- Menu -->
+							<div class="row d-none d-sm-flex mb-3 mb-lg-0 no-gutters">
+								<div class="col p-0">
+									<div class="row no-gutters">
+										<div class="p-0">
+										<a href="#1" class="home mt-3 py-2"><span>//01.</span>&lt;Main/&gt;</a>
+										<div class="w-100 d-table"></div>
+										<a href="#2" class="home mt-1 py-2"><span>//02.</span>&lt;Portfolio/&gt;</a>
+										</div>
+									</div>
+								</div>
+								<div class="col"></div>
+								<div class="col p-0">
+									<div class="row no-gutters">
+										<div class="col">
+											<div class="p-0 float-right">
+											<a href="#3" class="home text-right float-right mt-3 py-2"><span>//03.</span>&lt;Skills/&gt;</a>
+											<div class="w-100 d-table"></div>
+											<a href="#4" class="home text-right float-right mt-1 py-2"><span>//04.</span>&lt;Contacts/&gt;</a>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+							<!-- Main Text -->
+							<div class="row no-gutters align-items-start align-items-sm-center h-75">
+								<div class="col p-0">
+									<div class="row no-gutters mt-5 mt-md-0">
+										<div class="col p-0">
+											<div class="row no-gutters">
+												<p class="mb-lg-4 mb-2"><span>&lt;p&gt;</span>Hello, my name is<span>&lt;/p&gt;</span></p>
+											</div>
+											<div class="row no-gutters pl-3">
+												<div class="col m-sm-0 m-auto p-0">
+													<div class="row no-gutters">
+														<p><span>&lt;h1&gt;</span></p>
+													</div>
+													<div class="row no-gutters">
+														<h1 class="ml-3 d-none d-sm-block">
+															Korovin Novomir Vladimirovich
+														</h1>
+														<h1 class="ml-3 d-block d-sm-none">
+															Korovin Novomir
+														</h1>
+													</div>
+													<div class="row no-gutters">
+														<p><span>&lt;/h1&gt;</span></p>
+													</div>
+
+												</div>
+											</div>
+											<div class="row no-gutters mt-lg-4 mt-2">
+												<div class="col">
+													<p class="mb-sm-4 mb-3"><span>&lt;p&gt;</span>I am a professional programmer<span>&lt;/p&gt;</span></p>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="w-100 d-block mt-3  d-lg-none"></div>
+								<div class="col p-0 mb-0 mb-md-5 mb-lg-0">
+									<div class="button_scils m-auto">
+										<a href="#3">&lt;/Skills&gt;</a>
+										<div class="sycle" id="c_1"></div>
+										<div class="sycle" id="c_2"></div>
+										<div class="sycle" id="c_3"></div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="container-fluid mb-sm-0 mb-3" id="title_2">
+		<div class="row">
+			<div class="lines_myself">
+				<div id="l_1"></div>
+			</div>
+			<div class="col-7 px-0 col-md-9 py-4 m-auto">
+					<h2 class="text-center m-auto">My Portfolio</h2>
+			</div>
+		</div>
+	</div>
+	<div class="section" id="2">
+		<div class="lines" id="l_3"></div>
+		<div class="lines" id="l_4"></div>
+		<div class="lines" id="l_5"></div>
+		<div class="container-fluid h-100">
+			<div class="row no-gutters h-100" id="portfolio_box">
+			</div>
+		</div>
+	</div>
+	<div class="container-fluid pt-5">
+		<div class="row pt-5">
+			<div class="lines_myself">
+				<div id="l_2"></div>
+			</div>
+			<div class="col pt-5 m-auto">
+			</div>
+		</div>
+	</div>
+	<div class="section" id="3">
+		<div class="container-fluid p-0">
+			<div class="row no-gutters">
+				<div class="col">
+					<div class="row no-gutters pt-5">
+						<div class="col">
+							<h2 class="text-center m-auto" id="h_3">My Skills</h2>
+						</div>
+					</div>
+					<div class="row no-gutters pt-5">
+						<div class="col mt-4">
+							<div class="row no-gutters skill py-4">
+								<div class="col-10 m-auto">
+									<div class="row no-gutters align-items-center">
+										<div class="col">
+											<div class="row no-gutters">
+												<div class="col"><h4>Frontend Development </h4></div>
+											</div>
+											<div class="row no-gutters">
+												<div class="col"><p class="term">6 years of experience.</p></div>
+											</div>
+										</div>
+										<div class="col-1"></div>
+									</div>
+								</div>
+							</div>
+
+							<div class="row no-gutters skill py-4">
+								<div class="col-10 m-auto">
+									<div class="row no-gutters align-items-center">
+										<div class="col-1"></div>
+										<div class="col">
+											<div class="row no-gutters">
+												<div class="col"><h4>Css</h4></div>
+											</div>
+										</div>
+										<div class="col-4"><p class="percent">100%</p></div>
+									</div>
+								</div>
+							</div>
+
+							<div class="row no-gutters skill py-4">
+								<div class="col-10 m-auto">
+									<div class="row no-gutters align-items-center">
+										<div class="col-1"></div>
+										<div class="col">
+											<div class="row no-gutters">
+												<div class="col"><h4>Sass</h4></div>
+											</div>
+										</div>
+										<div class="col-4"><p class="percent">100%</p></div>
+									</div>
+								</div>
+							</div>
+
+							<div class="row no-gutters skill py-4">
+								<div class="col-10 m-auto">
+									<div class="row no-gutters align-items-center">
+										<div class="col-1"></div>
+										<div class="col">
+											<div class="row no-gutters">
+												<div class="col"><h4>Scss</h4></div>
+											</div>
+										</div>
+										<div class="col-4"><p class="percent">100%</p></div>
+									</div>
+								</div>
+							</div>
+
+							<div class="row no-gutters skill py-4">
+								<div class="col-10 m-auto">
+									<div class="row no-gutters align-items-center">
+										<div class="col-1"></div>
+										<div class="col">
+											<div class="row no-gutters">
+												<div class="col"><h4>JavaScript</h4></div>
+											</div>
+										</div>
+										<div class="col-4"><p class="percent">100%</p></div>
+									</div>
+								</div>
+							</div>
+
+							<div class="row no-gutters skill py-4">
+								<div class="col-10 m-auto">
+									<div class="row no-gutters align-items-center">
+										<div class="col-1"></div>
+										<div class="col">
+											<div class="row no-gutters">
+												<div class="col"><h4>ReactJs</h4></div>
+											</div>
+										</div>
+										<div class="col-4"><p class="percent">100%</p></div>
+									</div>
+								</div>
+							</div>
+
+							<div class="row no-gutters skill py-4">
+								<div class="col-10 m-auto">
+									<div class="row no-gutters align-items-center">
+										<div class="col-1"></div>
+										<div class="col">
+											<div class="row no-gutters">
+												<div class="col"><h4>JQuery</h4></div>
+											</div>
+										</div>
+										<div class="col-4"><p class="percent">100%</p></div>
+									</div>
+								</div>
+							</div>
+
+							
+
+							<div class="row no-gutters skill py-4">
+								<div class="col-10 m-auto">
+									<div class="row no-gutters align-items-center">
+										<div class="col">
+											<div class="row no-gutters">
+												<div class="col"><h4>Backend Development</h4></div>
+											</div>
+											<div class="row no-gutters">
+												<div class="col"><p class="term">6 years of experience.</p></div>
+											</div>
+										</div>
+										<div class="col-1"></div>
+									</div>
+								</div>
+							</div>
+
+							<div class="row no-gutters skill py-4">
+								<div class="col-10 m-auto">
+									<div class="row no-gutters align-items-center">
+										<div class="col-1"></div>
+										<div class="col">
+											<div class="row no-gutters">
+												<div class="col"><h4>Php</h4></div>
+											</div>
+										</div>
+										<div class="col-4"><p class="percent">100%</p></div>
+									</div>
+								</div>
+							</div>
+
+							<div class="row no-gutters skill py-4">
+								<div class="col-10 m-auto">
+									<div class="row no-gutters align-items-center">
+										<div class="col-1"></div>
+										<div class="col">
+											<div class="row no-gutters">
+												<div class="col"><h4>Yii2</h4></div>
+											</div>
+										</div>
+										<div class="col-4"><p class="percent">100%</p></div>
+									</div>
+								</div>
+							</div>
+
+							<div class="row no-gutters skill py-4">
+								<div class="col-10 m-auto">
+									<div class="row no-gutters align-items-center">
+										<div class="col-1"></div>
+										<div class="col">
+											<div class="row no-gutters">
+												<div class="col"><h4>WordPress</h4></div>
+											</div>
+										</div>
+										<div class="col-4"><p class="percent">100%</p></div>
+									</div>
+								</div>
+							</div>
+
+							<div class="row no-gutters skill b-green py-4">
+								<div class="col-10 m-auto">
+									<div class="row no-gutters align-items-center">
+										<div class="col-1"></div>
+										<div class="col">
+											<div class="row no-gutters">
+												<div class="col"><h4>Joomla</h4></div>
+											</div>
+										</div>
+										<div class="col-4"><p class="percent">100%</p></div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="container-fluid pt-5 pb-5">
+		<div class="row mt-5 pt-5">
+			<div class="lines_myself">
+				<div id="l_3"></div>
+			</div>
+		</div>
+	</div>
+
+	<div class="section" id="4">
+		<div class="container-fluid p-0 h-100">
+			<div class="row no-gutters">
+				<div class="col m-auto">
+					<div class="m-auto" id="line_contacts"></div>
+				</div>
+			</div>
+			<div class="row no-gutters h-100">
+				<div class="col-11 m-auto h-100">
+					<div class="row no-gutters pt-5 h-100">
+						<div class="col-11 m-auto h-100">
+							<div class="row no-gutters mb-5">
+								<div class="col">
+									<h2 class="text-center m-auto" id="h_4">Contacts</h2>
+								</div>
+							</div>
+
+								<div class="row no-gutters h-75 align-items-center">
+									<div class="col">
+										<div class="row no-gutters">
+											<div class="col-sm-6 col m-auto m-sm-0">
+												<div class="row no-gutters mb-3 m-sm-0">
+													<div class="col-sm-5 col m-sm-0 m-auto contact">
+														<a class="big m-auto" id="instagram" href="https://www.instagram.com/novomirkorovin/" target="_blank"></a>
+													</div>
+												</div>
+											</div>
+											<div class="w-100 d-sm-none d-block"></div>
+											<div class="col-sm-6 col m-sm-0 m-auto">
+												<div class="row no-gutters mb-3 m-sm-0">
+													<div class="col-sm-5 col m-sm-0 m-auto contact">
+														<a class="big m-auto" id="linkedin" href="https://www.linkedin.com/in/novomir-korovin-519298151" target="_blank"></a>
+													</div>
+												</div>
+											</div>
+										</div>
+										<div class="row no-gutters mb-md-3 mb-lg-0">
+											<div class="col-sm-6 col m-sm-0 m-auto">
+												<div class="row no-gutters mb-3 m-sm-0">
+													<div class="col d-sm-block d-none"></div>
+													<div class="col-sm-6 m-auto m-sm-0 col contact">
+														<a class="smol m-auto" id="twitter" href="https://twitter.com/JVUe6uWKf11RZ7X?t=8tyO_vVPVqfJE6LMZ65Pqw&s=09" target="_blank"></a>
+													</div>
+												</div>
+											</div>
+											<div class="w-100 d-sm-none d-block"></div>
+											<div class="col-sm-6 col">
+												<div class="row no-gutters mb-4 m-sm-0">
+													<div class="col d-sm-block d-none"></div>
+													<div class="w-100 d-sm-none d-block"></div>
+													<div class="col-sm-6 col m-sm-0 m-auto contact">
+														<a class="smol m-auto" id="gmail" href="mailto:novomir.work@gmail.com"></a>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+  <!-- Загрузим React. -->
+  <!-- Примечание: при деплое на продакшен замените «development.js» на «production.min.js». -->
+<script src="https://unpkg.com/react@17/umd/react.development.js" crossorigin></script>
+<script src="https://unpkg.com/react-dom@17/umd/react-dom.development.js" crossorigin></script>
+	<script src="web/js/portfolio.js"></script>
+	<script src="web/js/slick.min.js"></script>
+	<script src="web/js/main.js"></script>
+</body>
+</html>
